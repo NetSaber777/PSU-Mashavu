f98354b8e75994313055426df5ea3de762ebd3dd
S Tierney <NetSaber777@gmail.com>
2015-04-20 02:22:17 -0400

    Added main project folder and main.ino file

---
+++ main/main.ino/main.ino.ino
@@ -0,0 +1,15 @@
+//Main Mashavu Work Sketch
+/*Requires following work libraries in Arduino sketchbook libraries folder
+-
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
