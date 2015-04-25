2c5c69d2ea376ec91bbb2e4d24f83a662ed12d94
S Tierney <NetSaber777@gmail.com>
2015-04-25 05:51:36 -0400

    Began basic menu interface
    I'm slowly losing my mind.

--- main/Mashavu/Mashavu.ino
+++ main/Mashavu/Mashavu.ino
@@ -2,7 +2,7 @@
 /*Our Git Repo - https://github.com/NetSaber777/PSU-Mashavu
 
 Requires following work libraries in Arduino sketchbook libraries folder - http://www.arduino.cc/en/guide/libraries
--Adafruit RGB LCD Shield Library - https://github.com/adafruit/Adafruit-RGB-LCD-Shield-Library/archive/master.zip 
+-Adafruit RGB LCD Shield Library - https://github.com/adafruit/Adafruit-RGB-LCD-Shield-Library/archive/master.zip
 --Guide https://learn.adafruit.com/rgb-lcd-shield/overview
 -Adafruit Ultimate GPS Logger Shield - https://github.com/adafruit/Adafruit-GPS-Library/
 --Guide https://learn.adafruit.com/adafruit-ultimate-gps-logger-shield/overview
@@ -17,6 +17,7 @@ Requires following work libraries in Arduino sketchbook libraries folder - http:
 #include <Adafruit_GPS.h>
 #include <SPI.h>
 #include <SD.h>
+#include <EEPROM.h>
 
 //Begin DEV Settings
 bool DEBUG_STATE;
@@ -46,10 +47,10 @@ void useInterrupt(boolean);
 const byte ROWS = 4; //four rows
 const byte COLS = 3; //three columns
 char keys[ROWS][COLS] = {
- {'1','2','3'},
- {'4','5','6'},
- {'7','8','9'},
- {'*','0','#'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
 byte rowPins[ROWS] = {5, 6, 7, 8}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {2, 3, 4}; //connect to the column pinouts of the keypad
@@ -60,10 +61,13 @@ uint32_t timer = millis();
 
 //Program Data Structs
 typedef struct {
+  //Patient Data
   uint8_t gender, age, height, weight, systolic_bp, diastolic_bp;
   //GPS DATA
   uint8_t hour, minute, seconds, year, month, day;
-} SD_ENTRY;
+} ENTRY;
+
+ENTRY SD_QUEUE[1024];
 
 //Begin Program Logic
 
@@ -73,9 +77,9 @@ SIGNAL(TIMER0_COMPA_vect) {
   // if you want to debug, this is a good time to do it!
 #ifdef UDR0
   if (GPSECHO && DEBUG_STATE)
-    if (c) UDR0 = c;  
-    // writing direct to UDR0 is much much faster than Serial.print 
-    // but only one character can be written at a time. 
+    if (c) UDR0 = c;
+  // writing direct to UDR0 is much much faster than Serial.print
+  // but only one character can be written at a time.
 #endif
 }
 
@@ -93,36 +97,25 @@ void useInterrupt(boolean v) {
   }
 }
 
-void setup() {
-  //delay(5000);
-  //Serial Debug via USB. Higher Refresh to keep up with parsing rate of GPS to USB Buffer
-  Serial.begin(115200);
-  Serial.println("Begin Mashavu Debug");
-  useInterrupt(true);
-  DEBUG_STATE = true;
-  
-  //Begin Serial1 Interface on GPS Module
-  GPS.begin(9600);
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-  GPS.sendCommand(PGCMD_ANTENNA);
-  
-  //Begin LCD Screen - I2C Connection
-  // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
-  lcd.setBacklight(WHITE);
-  lcd.print("Hello, Scrub!");
-  delay(3000);
+//Shows current GPS time on LCD screen
+void lcdShowTime() {
+  lcd.clear();
+  lcd.print("Time");
+  lcd.setCursor(0, 1);
+
+  lcd.print(GPS.hour); lcd.print(":");
+  lcd.print(GPS.minute); lcd.print(":");
+  lcd.print(GPS.seconds);
 }
 
-void gpsDebug(){
+void gpsDebug() {
   // if millis() or timer wraps around, we'll just reset it
   if (timer > millis())  timer = millis();
 
   // print state every 2 seconds
-  if (millis() - timer > 2000) { 
+  if (millis() - timer > 2000) {
     timer = millis(); // reset the timer
-    
+
     Serial.print("\nTime: ");
     Serial.print(GPS.hour, DEC); Serial.print(':');
     Serial.print(GPS.minute, DEC); Serial.print(':');
@@ -133,17 +126,17 @@ void gpsDebug(){
     Serial.print(GPS.month, DEC); Serial.print("/20");
     Serial.println(GPS.year, DEC);
     Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
     if (GPS.fix) {
       Serial.print("Location: ");
       Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", "); 
+      Serial.print(", ");
       Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
       Serial.print("Location (in degrees, works with Google Maps): ");
       Serial.print(GPS.latitudeDegrees, 4);
-      Serial.print(", "); 
+      Serial.print(", ");
       Serial.println(GPS.longitudeDegrees, 4);
-      
+
       Serial.print("Speed (knots): "); Serial.println(GPS.speed);
       Serial.print("Angle: "); Serial.println(GPS.angle);
       Serial.print("Altitude: "); Serial.println(GPS.altitude);
@@ -152,21 +145,85 @@ void gpsDebug(){
   }
 }
 
-//Shows current GPS time on LCD screen
-void lcdShowTime(){
-  lcd.clear();
-  lcd.print("Time");
-  lcd.setCursor(0,1);
+//Patient Data Types uint8_t gender, age, height, weight, systolic_bp, diastolic_bp;
+void userEntry() {
+  //Create Data Entry Object
+  //ENTRY patient = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int patient[6];
+  uint8_t buttons;
+  bool NEXT;
+  for(uint8_t i=0; i<6; i++){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(DATA_ENTRY[i]);
+    lcd.setCursor(0,1);
+    NEXT = false;
+    char buff;
+    while(NEXT == false){
+       buff = keypad.getKey();
+       if(buttons & BUTTON_SELECT){
+         patient[i] = 
+         NEXT = true;
+       }
+    }
+  }
   
-  lcd.print(GPS.hour); lcd.print(":");
-  lcd.print(GPS.minute); lcd.print(":");
-  lcd.print(GPS.seconds);
+  /* - Menu System Work in Progress
+  bool SUBMIT = false;
+  uint8_t MENU_INDEX = 0;
+  while (SUBMIT == false) {
+    uint8_t buttons = lcd.readButtons();
+    if (buttons) {
+      if (buttons & BUTTON_UP) {
+        if (MENU_INDEX < 4 ) {
+          MENU_INDEX++;
+        }
+      }
+      if (buttons & BUTTON_DOWN) {
+        if (MENU_INDEX > 0 ) {
+          MENU_INDEX--;
+        }
+      }
+      if (buttons & BUTTON_LEFT) {
+
+      }
+      if (buttons & BUTTON_RIGHT) {
+
+      }
+      if (buttons & BUTTON_SELECT) {
+
+      }
+    }
+  }*/
+}
+
+void setup() {
+  //delay(5000);
+  //Serial Debug via USB. Higher Refresh to keep up with parsing rate of GPS to USB Buffer
+  Serial.begin(115200);
+  Serial.println("Begin Mashavu Debug");
+  useInterrupt(true);
+  DEBUG_STATE = true;
+
+  //Begin SD Card SPI Interface-  COMPLETELY FUBAR WITH SERIAL INTERRUPTS ON GPS
+
+  //Begin Serial1 Interface on GPS Module
+  GPS.begin(9600);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  //Begin LCD Screen - I2C Connection
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  lcd.setBacklight(WHITE);
+  lcd.print(MAIN_MENU[0]);
+  delay(3000);
 }
 
 
-
 void loop() {
-  
+
   // Alternative to Interrupt Handling. - Leave in
   if (! usingInterrupt) {
     // read data from the GPS in the 'main loop' - not active now
@@ -175,18 +232,20 @@ void loop() {
     if (GPSECHO && DEBUG_STATE)
       if (c) Serial.print(c);
   }
-  
+
   // if NMEA sentece receievevd, parse away
   if (GPS.newNMEAreceived()) {
     if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
       return;  // parse fail handling - and we don't care
   }
 
-  if(DEBUG_STATE)
+  if (DEBUG_STATE) {
     gpsDebug();
+  }
   
-  lcdShowTime();
-  delay(1000);
+  userEntry();
+  //lcdShowTime();
+  //delay(1000);
   /*
   char key = keypad.getKey();
 
